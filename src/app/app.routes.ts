25ed124ff1307a0181e486a0c48a3bb7953bbb75
Loek Appel <loek@appel-snijder.nl>
2024-01-03 14:50:53 +0100

    begin login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { RegistrationFormComponent } from "./login/registration-form/registration-form.component";
+import { AccountComponent } from "./login/account/account.component";
+import {NgModule} from "@angular/core";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
+    {path: 'registration', component: RegistrationFormComponent},
+    {path: 'account', component: AccountComponent},
+    {path: '**', redirectTo: 'not-found', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+
+export class AppRoutingModule { }
